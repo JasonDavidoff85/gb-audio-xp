f2a5ba347d37f3ffec51033d397e2f95a6e5db3e
JasonDavidoff <jasondavidoff85@gmail.com>
2026-05-13 15:08:30 -0600

    update name, add definitions

--- project.mk
+++ project.mk
@@ -11,10 +11,10 @@ PADVALUE := 0xFF
 VERSION := 0
 
 # 4-ASCII letter game ID.
-GAMEID := MUSE
+GAMEID := NOIZ
 
 # Game title, up to 11 ASCII chars.
-TITLE := MUSE
+TITLE := GB-Noise
 
 # New licensee, 2 ASCII chars.
 # Homebrew games FTW!.
@@ -36,7 +36,7 @@ MBC := 0x00
 SRAMSIZE := 0x00
 
 # ROM name.
-ROMNAME := muse
+ROMNAME := gb-noise
 ROMEXT  := gb
 
 
